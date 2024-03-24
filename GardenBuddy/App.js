0b7c7aa3aac834302b5900fe92dd6a23fb7212b4
Irene Lie <77621546+ireneliee@users.noreply.github.com>
2024-03-25 01:50:40 +0800

    add ui component library

--- GardenBuddy/App.js
+++ GardenBuddy/App.js
@@ -1,13 +1,17 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider>
+      <View style={styles.container}>
       <Text>Welcome to GardenBuddy~!</Text>
       <Text>This is a new page</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaProvider>
+    
   );
 }
 
