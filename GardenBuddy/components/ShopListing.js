0fd2a140670d41f84735bdb40213c5d947ca59d8
kohwenjie <70895902+kohwenjie@users.noreply.github.com>
2024-04-01 16:48:18 +0800

    added shop retrieve data

---
+++ GardenBuddy/components/ShopListing.js
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { Card } from "@rneui/themed";
+
+const windowWidth = Dimensions.get("window").width;
+const cardWidth = windowWidth / 2 - 20;
+const cardHeight = cardWidth * 1;
+const imageWidth = cardWidth * 0.9;
+const imageHeight = cardHeight * 0.9;
+
+const placeHolderLink = require("../assets/plantPlaceholder.jpg");
+
+const ShopListing = ({ shopItemList, onSelectListing }) => {
+  const handleSelect = (id) => {
+    onSelectListing(id);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleSelect(item.id)}>
+      <Card containerStyle={styles.itemContainer}>
+        <Image
+          source={placeHolderLink}
+          style={[
+            styles.itemImage,
+            { width: imageWidth, height: imageHeight, margin: "auto" },
+          ]}
+          resizeMode="contain"
+        />
+        <View style={styles.itemDetails}>
+          <Text style={styles.itemName}>{item.name}</Text>
+          <Text style={styles.itemPrice}>Price: ${item.price}</Text>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+
+  return (
+    <FlatList
+      data={shopItemList}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id.toString()}
+      numColumns={2}
+      contentContainerStyle={styles.container}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  itemContainer: {
+    flex: 1,
+    margin: 5,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    width: cardWidth,
+    height: cardHeight,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  itemImage: {
+    marginBottom: 10,
+  },
+  itemDetails: {
+    flex: 1,
+    alignItems: "center",
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  itemDescription: {
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  itemPrice: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  itemQuantity: {
+    fontStyle: "italic",
+  },
+});
+
+export default ShopListing;
--- GardenBuddy/pages/Shop.js
+++ GardenBuddy/pages/Shop.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
 import { ShopApi } from "../api/Api";
+import { ButtonGroup } from "react-native-elements";
+import ShopListing from "../components/ShopListing";
+
 const Shop = () => {
   const [accessories, setAccessories] = useState([]);
   const [gardenBuddyPacks, setGardenBuddyPacks] = useState([]);
+  const [selectedTab, setSelectedTab] = useState(0);
 
   useEffect(() => {
     getData();
@@ -11,29 +14,36 @@ const Shop = () => {
 
   const getData = async () => {
     const response = await ShopApi.getAllAccessories();
-    console.log(response.data);
     setAccessories(response.data);
 
     const response2 = await ShopApi.getAllGardenBuddyPacks();
-    console.log(response2.data);
     setGardenBuddyPacks(response2.data);
   };
 
   return (
-    <View>
-      <Text>This is your shop</Text>
-      <>testest</>
+    <>
+      <ButtonGroup
+        buttons={["View Packs", "View Accessory"]}
+        selectedIndex={selectedTab}
+        onPress={(value) => {
+          setSelectedTab(value);
+        }}
+        containerStyle={{ marginBottom: 20, height: 20 }}
+      />
 
-      {accessories &&
-        accessories.map((acc) => {
-          return <>{acc.id}</>;
-        })}
-
-      {gardenBuddyPacks &&
-        gardenBuddyPacks.map((pack) => {
-          return <>{pack.id}</>;
-        })}
-    </View>
+      {selectedTab === 0 && (
+        <ShopListing
+          shopItemList={gardenBuddyPacks}
+          onSelectListing={(id) => postSelecting(id)}
+        />
+      )}
+      {selectedTab === 1 && (
+        <ShopListing
+          shopItemList={accessories}
+          onSelectListing={(id) => postSelecting(id)}
+        />
+      )}
+    </>
   );
 };
 
